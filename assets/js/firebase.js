c7a09a50b3bb89832c1048e74566aece38faae85
anand raj <rajanand9039@gmail.com>
2021-12-24 20:35:21 +0530

    firebase crud with vanilla js

---
+++ assets/js/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCXm9N8-WdSFemEhNDVPSU1hFGMELdHBFg",
+    authDomain: "ott-buddy.firebaseapp.com",
+    projectId: "ott-buddy",
+    storageBucket: "ott-buddy.appspot.com",
+    messagingSenderId: "336993760040",
+    appId: "1:336993760040:web:74f3bc87e474d51081561d"
+  };
